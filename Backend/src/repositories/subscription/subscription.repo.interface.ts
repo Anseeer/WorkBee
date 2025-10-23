1291ad470a25762d525b3b4e0676579aaa93e294
AHMED ANSEER AD <ahmedanseer007@gmail.com>
2025-10-23 22:19:09 +0530

    Save current changes

--- Backend/src/repositories/subscription/subscription.repo.interface.ts
+++
@@ -1,9 +0,0 @@
-import { ISubscription } from "../../model/subscription/subscription.interface";
-import { Iread, Iwrite } from "../base/base.repo.interface";
-
-export interface ISubscriptionRepository extends Iwrite<ISubscription>, Iread<ISubscription> {
-    find(status?: boolean): Promise<ISubscription[]>;
-    findByName(name: string): Promise<ISubscription | null>;
-    toggleStatus(subscriptionId: string): Promise<void>;
-    update(subscriptionId: string, subscriptionData: Partial<ISubscription>): Promise<ISubscription>;
-}
